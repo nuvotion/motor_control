2d181906c434983d596c053a87f4b276a487d684
Kim Taylor <you@example.com>
2018-08-11 16:23:18 +1000

    Bootloader building.

---
+++ bootloader/build/lib.mk
@@ -0,0 +1,16 @@
+
+CYPRESS_SRCDIR  := ../config
+CYPRESS_C_SRC   := $(notdir $(wildcard $(CYPRESS_SRCDIR)/*.c))
+CYPRESS_A_SRC   := $(notdir $(CYPRESS_SRCDIR)/CyBootAsmGnu.s)
+CYPRESS_OBJ     := $(CYPRESS_C_SRC:.c=.o) $(CYPRESS_A_SRC:.s=.o)
+
+INCLUDES := -I $(CYPRESS_SRCDIR)
+
+$(TARGET).a: $(CYPRESS_OBJ)
+	$(AR) -rs $@ $^
+
+%.o:$(CYPRESS_SRCDIR)/%.c
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+%.o:$(CYPRESS_SRCDIR)/%.s
+	$(AS) $(ASMFLAGS) $(INCLUDES) $< -o $@ 
